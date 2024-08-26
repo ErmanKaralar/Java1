0d4a147b2a4f828fab9d18befc48cd815885c34a
ErmanKaralar <karalarerman@gmail.com>
2024-08-26 15:19:49 +0300

    Sepet Testi eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -7,7 +7,10 @@ public class Merhaba {
         System.out.println("Merhaba 2");
 
         System.out.println("Task 1 kodları");
+
+        System.out.println("Task 2 kodları");
     }
 }
 //Kırmızı renk: Bu dosya henüz takip edilmiyor
 //Yeşil renk: Bu dosya  takip ediliyor, yani değişiklikler kaydedilecek
+//commit: O zamana kadar yaptığın değişikliklere bir isim vermek demektir
