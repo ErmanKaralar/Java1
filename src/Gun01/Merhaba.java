9316df354d2733b611817bc4f3dc3ee1d985acbd
ErmanKaralar <karalarerman@gmail.com>
2024-08-26 15:11:36 +0300

    Login Testi eklendi(Sana gelen işin adı, Jiradan gelen)

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,13 @@
+package Gun01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+        System.out.println("Merhaba 1");
+
+        System.out.println("Merhaba 2");
+
+        System.out.println("Task 1 kodları");
+    }
+}
+//Kırmızı renk: Bu dosya henüz takip edilmiyor
+//Yeşil renk: Bu dosya  takip ediliyor, yani değişiklikler kaydedilecek
